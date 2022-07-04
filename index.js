5f51aabca44f6818d2deb8a2a2fa3754db879c70
Tim <tim.fink519@gmail.com>
2022-07-03 20:06:38 -0400

    commit tree 3b915ff0b96595c6805f471a800792361f034a88

--- index.js
+++ index.js
@@ -1,11 +1,41 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(ride){
+  let cost
+  if (ride <= 400) {
+    cost = 'This one is on me!';
+  } else if (ride > 400 && ride < 2000) {
+    cost = 'That will be twenty bucks.';
+  } else if (ride > 2000 && ride < 2500) {
+    cost = 'I will gladly take your thirty bucks.';
+  } else if (ride > 2500) {
+    cost = 'No can do.';
+  }
+  return cost;
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+  let result;
+  return result = (city === 'NYC') ? 'Ok, sounds good.' : 'No go.';
+   
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+ 
+  switch (tip) {
+    case 'generous': {
+      tip = "Thank you so much."
+      console.log(tip);
+      break;
+    }
+    case 'not as generous': {
+      tip = "Thank you.";
+      console.log(tip);
+      break;
+    }
+    default: {
+      tip = "Bye.";
+      console.log(tip);
+      break;
+    }
+  }
+  return tip;
 }
